e1be6478d1d01b1951267fb146e84b3c5355b9d1
Ali Bidkhori <a.bidkhori2004@gmail.com>
2023-10-31 13:34:43 +0100

    1.0.1 - Added Redirect

--- website-app/src/App.js
+++ website-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/homepage" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
       </Router>
     </div>
